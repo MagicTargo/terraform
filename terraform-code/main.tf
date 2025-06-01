fcbf62defd8d5cfba06cac2300a3fb43e38d88b2
MagicTargo <sunnymohite1993@gmail.com>
2025-06-01 21:08:30 +0530

    logic change

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -1,10 +1,25 @@
-resource "github_repository_file" "flux_kustomization" {
-  repository          = var.repository_full_name
-  branch              = "main"
-  file                = join("/", ["clusters", var.environment, "kustomization.yaml"])
-  content             = "hello world"
-  commit_message      = "Kustomization for via TF"
-  commit_author       = "Terraform GitHub Actions"
-  commit_email        = "terraform-gha@apollo.com"
-  overwrite_on_create = false
+resource "null_resource" "check_file_exists" {
+  provisioner "local-exec" {
+    command = <<EOT
+      gh api repos/${var.repository_full_name}/contents/clusters/${var.environment}/kustomization.yaml \
+        --jq .sha 2>/dev/null || \
+      gh api repos/${var.repository_full_name}/contents/clusters/${var.environment}/kustomization.yaml \
+        --method PUT \
+        --field message='Kustomization via TF' \
+        --field content='$(echo -n "hello world" | base64)' \
+        --field branch='main'
+    EOT
+  }
+  
 }
+
+# resource "github_repository_file" "flux_kustomization" {
+#   repository          = var.repository_full_name
+#   branch              = "main"
+#   file                = join("/", ["clusters", var.environment, "kustomization.yaml"])
+#   content             = "hello world"
+#   commit_message      = "Kustomization for via TF"
+#   commit_author       = "Terraform GitHub Actions"
+#   commit_email        = "terraform-gha@apollo.com"
+#   overwrite_on_create = false
+# }
