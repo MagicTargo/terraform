7be0ec3622c7632f751dc182f143c4d450cc3867
MagicTargo <sunnymohite1993@gmail.com>
2025-06-02 21:20:08 +0530

    !wqS

--- scripts/create_kustomization.sh
+++ scripts/create_kustomization.sh
@@ -14,25 +14,21 @@ REPO_NAME="${OWNER}/${REPO%.git}"
 FILE="${KUSTOMIZATION_PATH}/kustomization.yaml"
 CONTENT=$(echo -n "hello world" | base64)
 
-# Define lock file based on the repo name to prevent simultaneous access
+# using lockfile to prevent simultaneous access
 LOCKFILE="/tmp/$(basename "$GIT_URL").lock"
 
 echo "Checking for file: ${FILE} in repo: ${REPO_NAME}"
 
-# Acquire the lock using flock to ensure only one script modifies the repo at a time
-flock "$LOCKFILE" -c '
-  # Checking if the file exists in the repo
-  if gh api "repos/${REPO_NAME}/contents/${FILE}" --jq .sha > /dev/null 2>&1; then
-    echo "File already exists: ${FILE}"
+# using flock to aquire lock to ensure only one script modifies the repo at a time
+flock "$LOCKFILE" -c "
+  if gh api repos/${REPO_NAME}/contents/${FILE} --jq .sha > /dev/null 2>&1; then
+    echo 'File already exists: ${FILE}'
   else
-    echo "Kustomization file not found. Creating file: ${FILE}"
-    echo "gh api repos/${REPO_NAME}/contents/${FILE}"
-    # Create the file using GitHub API with the base64 content
-    gh api "repos/${REPO_NAME}/contents/${FILE}" \
+    echo 'Creating file: ${FILE}'
+    gh api repos/${REPO_NAME}/contents/${FILE} \
       --method PUT \
-      --field message="Kustomization created via TF" \
-      --field content="${CONTENT}" \
-      --field branch="${BRANCH}"
-    echo "File created"
+      --field message='Kustomization created via TF' \
+      --field content='${CONTENT}' \
+      --field branch='${BRANCH}'
   fi
-'
+"
