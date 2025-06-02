65d00fb1e13269587dc5d14021cb54e87f8cf4eb
MagicTargo <sunnymohite1993@gmail.com>
2025-06-02 17:31:53 +0530

    test

--- terraform-code/local.tf
+++ terraform-code/local.tf
@@ -1,11 +1,11 @@
 locals {
   flux_conf = {
     bootstrap = {
-      git_repo_url       = "https://github.com/MagicTargo/terraform.git"
+      git_repo_url       = "https://github.com/MagicTargo/FirstRepo.git"
       kustomization_path = join("/", ["clusters", "dev", "bootstrap"])
     }
     core = {
-      git_repo_url       = "https://github.com/MagicTargo/terraform.git"
+      git_repo_url       = "https://github.com/MagicTargo/FirstRepo.git"
       kustomization_path = join("/", ["clusters", "stg", "main"])
     }
   }
