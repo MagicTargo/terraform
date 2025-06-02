66939fe5d927af0fc85ab1d34ec0c44323339146
MagicTargo <sunnymohite1993@gmail.com>
2025-06-02 19:43:00 +0530

    t

--- modules/common/flux_infra/main.tf
+++ modules/common/flux_infra/main.tf
@@ -4,4 +4,9 @@ resource "terraform_data" "check_kustomization_file" {
   provisioner "local-exec" {
     command = "bash ${path.root}/../scripts/create_kustomization.sh ${each.value.git_repo_url} ${each.value.kustomization_path}"
   }
+
+  # Dynamically depend on previous execution in the loop (if applicable)
+  depends_on = each.key != "bootstrap" ? [
+    terraform_data.check_kustomization_file[local.flux_conf_keys[lookup(local.flux_conf, each.key) - 1]]
+  ] : []
 }
