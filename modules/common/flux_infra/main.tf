238296ec612511c1973d0f17f97b4bf2050a487b
MagicTargo <sunnymohite1993@gmail.com>
2025-06-02 16:47:20 +0530

    test

--- modules/common/flux_infra/main.tf
+++ modules/common/flux_infra/main.tf
@@ -2,6 +2,6 @@ resource "terraform_data" "check_kustomization_file" {
   for_each = var.flux_conf
 
   provisioner "local-exec" {
-    command = "bash ${path.module}/../scripts/create_kustomization.sh ${each.value.git_repo_url} ${each.value.kustomization_path}"
+    command = "bash ${path.root}/../scripts/create_kustomization.sh ${each.value.git_repo_url} ${each.value.kustomization_path}"
   }
 }
