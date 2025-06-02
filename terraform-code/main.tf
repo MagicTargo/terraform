ef131e8117e04f002b5f943295fb6c965eaa1dcb
MagicTargo <sunnymohite1993@gmail.com>
2025-06-02 16:40:56 +0530

    fmt

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -1,5 +1,5 @@
 module "cluster_infra" {
   source = "git::https://github.com/MagicTargo/terraform.git//modules/common/flux_infra?ref=main"
-  
-  flux_conf= local.flux_conf
+
+  flux_conf = local.flux_conf
 }
